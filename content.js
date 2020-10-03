7990e6561b9aede0f3a935d6c55ef63e6519d31e
Urmi Lalchandani <ulalchan@ucdavis.edu>
2020-10-03 14:21:09 -0700

    hello there at the top of the page

--- content.js
+++ content.js
@@ -1,9 +1,10 @@
 console.log(document);
-console.log('ajdklfsjkld', window.location);
+console.log(window.location);
 if (window.location.href.indexOf('target.com/c/fresh-flowers') > -1) {
     console.log('in here');
+    var elem = document.createElement("div");
+    var mainC = document.getElementById("root");
+    elem.appendChild(document.createTextNode("Hello there!"));
+    document.body.insertBefore(elem, mainC);
 }
 
-var elem = document.createElement("div");
-elem.appendChild(document.createTextNode("Hello there!"));
-document.body.appendChild(elem);
