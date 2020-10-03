93ab9ea135b47e6ef1799488ff83ac471681908c
Urmi Lalchandani <ulalchan@ucdavis.edu>
2020-10-03 15:30:01 -0700

    small changes added for loop

--- content.js
+++ content.js
@@ -1,10 +1,14 @@
 console.log(document);
 console.log(window.location);
-if (window.location.href.indexOf('target.com/c/fresh-flowers') > -1) {
-    console.log('in here');
-    var elem = document.createElement("div");
-    var mainC = document.getElementById("root");
-    elem.appendChild(document.createTextNode("Hello there!"));
-    document.body.insertBefore(elem, mainC);
-}
+
+var keywords = ['fresh-flowers', 'grocery', 'furniture', 'shoes', 'movies-music-books', 'beauty'];
+keywords.forEach(term => {
+    if (window.location.href.indexOf('target.com/c/'+term) > -1) {
+        console.log('in here');
+        var elem = document.createElement("div");
+        var mainC = document.getElementById("root");
+        elem.appendChild(document.createTextNode("Hello there!"));
+        document.body.insertBefore(elem, mainC);
+    }
+});
 
