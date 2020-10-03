3f18e1423695ab23452e34764220798311831f38
Urmi Lalchandani <ulalchan@ucdavis.edu>
2020-10-03 14:00:34 -0700

    based on content.js now

--- content.js
+++ content.js
@@ -1,1 +1,9 @@
-alert("you are looking for flowers, can i interest you in these stores instead?");
+console.log(document);
+console.log('ajdklfsjkld', window.location);
+if (window.location.href.indexOf('target.com/c/fresh-flowers') > -1) {
+    console.log('in here');
+}
+
+var elem = document.createElement("div");
+elem.appendChild(document.createTextNode("Hello there!"));
+document.body.appendChild(elem);
